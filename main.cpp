26a6302f13fc980848942f88108bbf978ac2f910
Ehsan Shams <ehsan@debian.home_network>
2020-05-23 13:08:57 -0700

    error handling first commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <assert.h>
 #include <mpi.h>
+using namespace std;
 
 class Communication {
 
@@ -10,9 +11,9 @@ public:
 
 		MPI_Init(&argc, &argv);
 
-		MPI_Comm_rank(MPI_COMM_WORLD, &_myrank);
+		MPI_Comm_rank(_comm, &_myrank);
 
-		MPI_Comm_size(MPI_COMM_WORLD, &_numprocs);
+		MPI_Comm_size(_comm, &_numprocs);
 
 		if (_myrank==0) 
 			std::cout << "Communication constructed with " << _numprocs << " procs!\n";
@@ -21,24 +22,40 @@ public:
 
 	int getRank() {return _myrank;}
 
-	int handShake(int source, int dest, int *buf, int tag) {
+	int handShake(int source, int dest, int *buf, int nbuf, int tag) {
 
-		int ierr = 0;
+		int ierr;
 
 		assert(source >= 0);
 		assert(dest >= 0);
 		assert(source < _numprocs);
 		assert(dest < _numprocs);
 
-		if (_myrank == source) 
-			ierr = MPI_Send(buf, sizeof(buf), MPI_INT, dest, tag, MPI_COMM_WORLD);
+		if (_myrank == source)  {
+			ierr = MPI_Send(buf, nbuf, MPI_INT, dest, tag, _comm);
+			errorCheck(ierr);
+		}
 
 		if (_myrank == dest) {
-			MPI_Status *status;
-			ierr = MPI_Recv(buf, sizeof(buf), MPI_INT, source, tag, MPI_COMM_WORLD, status);
+			ierr = MPI_Recv(buf, nbuf, MPI_INT, source, tag, _comm, MPI_STATUS_IGNORE);
+			errorCheck(ierr);
 		}
 
 		return ierr;
+	}
+
+	void errorCheck(int ierr) {
+
+		switch (ierr) {
+		case MPI_SUCCESS:
+		case MPI_ERR_COMM:
+		/*
+			 place holder
+		 */
+		default :
+			break;
+		}
+
 	}
 
 	~Communication() {
@@ -49,6 +66,7 @@ private:
 
 	int _myrank {0};
 	int _numprocs {1};
+	MPI_Comm _comm {MPI_COMM_WORLD};
 };
 
 int main(int argc, char **argv) {
@@ -56,6 +74,7 @@ int main(int argc, char **argv) {
 	Communication comm(argc,argv);
 
 	int a[16];
+	int nbuf = 4;
 
 	if (comm.getRank() == 0) {
 		a[0] = 1; 
@@ -66,7 +85,14 @@ int main(int argc, char **argv) {
 
 	int ierr = 0;
 
-	ierr = comm.handShake(0,1,a,0);
+	ierr = comm.handShake(0,1,a,nbuf,0);
+
+	if (comm.getRank() == 1) {
+		cout << '[' << comm.getRank() << "] ";
+		for (int i=0; i < nbuf; i++)
+			cout << a[i] << ' ';
+		cout << endl;
+	}
 
 	return 0;
 
