10bf86d6dbfd2345d332e4f1a4ef1e16b839fcf3
Ehsan Shams <ehsan@debian.home_network>
2020-05-23 12:15:55 -0700

    Added handshake

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <assert.h>
 #include <mpi.h>
 
 class Communication {
@@ -18,6 +19,28 @@ public:
 
 	}
 
+	int getRank() {return _myrank;}
+
+	int handShake(int source, int dest, int *buf, int tag) {
+
+		int ierr = 0;
+
+		assert(source >= 0);
+		assert(dest >= 0);
+		assert(source < _numprocs);
+		assert(dest < _numprocs);
+
+		if (_myrank == source) 
+			ierr = MPI_Send(buf, sizeof(buf), MPI_INT, dest, tag, MPI_COMM_WORLD);
+
+		if (_myrank == dest) {
+			MPI_Status *status;
+			ierr = MPI_Recv(buf, sizeof(buf), MPI_INT, source, tag, MPI_COMM_WORLD, status);
+		}
+
+		return ierr;
+	}
+
 	~Communication() {
 		MPI_Finalize();
 	}
@@ -31,6 +54,19 @@ private:
 int main(int argc, char **argv) {
 
 	Communication comm(argc,argv);
+
+	int a[16];
+
+	if (comm.getRank() == 0) {
+		a[0] = 1; 
+		a[1] = 2; 
+		a[2] = 3; 
+		a[3] = 4; 
+	}
+
+	int ierr = 0;
+
+	ierr = comm.handShake(0,1,a,0);
 
 	return 0;
 
